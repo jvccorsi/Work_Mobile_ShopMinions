2413de9712b053a62bd44710679e46c29ab0a543
João Victor Corsi Ferreira <32347104+jvccorsi@users.noreply.github.com>
2021-04-30 16:50:58 -0300

    casdatro finalizado

--- shop_minions/lib/model/cadastro.dart
+++ shop_minions/lib/model/cadastro.dart
@@ -2,9 +2,13 @@ class Cadastro {
   String username;
   String password;
   bool confirmed = false;
+  var radioValue = 1;
+  double sliderValue = .1;
 
   doSomething() {
     print("Username: $username");
     print("Password: $password");
+    print("radioValue: $radioValue");
+    print("Slider: $sliderValue");
   }
 }
