7692b928a68850eda7ec7e3f30a22dfcc4f4660d
João Victor Corsi Ferreira <32347104+jvccorsi@users.noreply.github.com>
2021-04-30 13:45:35 -0300

    cadastro inicial

---
+++ shop_minions/lib/model/cadastro.dart
@@ -0,0 +1,10 @@
+class Cadastro {
+  String username;
+  String password;
+  bool confirmed = false;
+
+  doSomething() {
+    print("Username: $username");
+    print("Password: $password");
+  }
+}
--- shop_minions/lib/view/tela_cadastro/tela_cadastro_main.dart
+++ shop_minions/lib/view/tela_cadastro/tela_cadastro_main.dart
@@ -1,7 +1,18 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:shop_minions/model/cadastro.dart';
 
-class MyTelaCadastro extends StatelessWidget {
+Cadastro cadastro_class = new Cadastro();
+
+class MyTelaCadastro extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return TelaCadastroState();
+  }
+}
+
+class TelaCadastroState extends State<MyTelaCadastro> {
+  GlobalKey<FormState> formKey = new GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,8 +35,55 @@ class MyTelaCadastro extends StatelessWidget {
           ),
         ),
       ),
+      backgroundColor: Color.fromRGBO(253, 217, 0, 1),
       body: Container(
-        color: Color.fromRGBO(253, 228, 65, 1),
+        padding: const EdgeInsets.all(40.0),
+        child: Center(
+          child: Column(
+            children: [
+              Form(
+                key: formKey,
+                child: Column(
+                  children: [
+                    usernameFormField(),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget usernameFormField() {
+    return Container(
+      width: 350,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(29),
+      ),
+      child: TextFormField(
+        keyboardType: TextInputType.name,
+        style: TextStyle(
+          fontFamily: 'PT Sans bold',
+          fontSize: 20,
+          color: Color.fromRGBO(116, 128, 139, 1),
+        ),
+        decoration: InputDecoration(
+          hintText: "Nome completo",
+          icon: Icon(
+            Icons.person,
+            color: Color.fromRGBO(116, 128, 139, 1),
+            size: 30,
+          ),
+          contentPadding: EdgeInsets.fromLTRB(2.0, 15, 20, 15),
+          border: InputBorder.none,
+          focusedBorder: InputBorder.none,
+          enabledBorder: InputBorder.none,
+          errorBorder: InputBorder.none,
+          disabledBorder: InputBorder.none,
+        ),
       ),
     );
   }
